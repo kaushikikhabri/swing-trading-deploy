9fa9f81504193bbb05d65d7252e88e55a40b102b
kaushiki_khabri <kaushiki.khabri@gmail.com>
2024-10-21 11:06:50 +0530

    Changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home"; // Import your Home component
 import Login from "./components/Login"; // Import your Login component
 import Register from "./components/Register"; // Import your Register component
